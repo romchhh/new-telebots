3328b40f8d261cbba9f51f0d691b69621e8c496f
nowayrm <your.email@example.com>
2026-03-13 00:27:16 +0200

    some updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,6 +150,13 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Analytics />
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vutiawpnrs");`}
+        </Script>
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <>
             <Script
