842814ecb741c02add79fe8eec9a06ce98001360
nowayrm <your.email@example.com>
2026-04-22 16:51:08 +0300

    some updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -166,8 +166,27 @@ export default function RootLayout({
         <meta name="revisit-after" content="7 days" />
         <meta name="rating" content="general" />
         <meta name="mobile-web-app-capable" content="yes" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6GS5CQC');`,
+          }}
+        />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6GS5CQC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <SpeedInsights />
         <Analytics />
